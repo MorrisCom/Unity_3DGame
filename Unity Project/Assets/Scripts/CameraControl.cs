cd70e98f96c1a656341a2f4276ec5e68222fe365
MorrisCom <57181186+MorrisCom@users.noreply.github.com>
2019-12-23 15:38:31 +0800

    攝影機設定及角色移動

---
+++ Unity Project/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    #region
+    private Transform player;
+
+    [Header("追蹤速度"),Range(1,10)]
+    public float speed = 5;
+    [Header("上方限制"),]
+    public float top = -6;
+    [Header("下方限制"),]
+    public float bottom = 6;
+    #endregion
+    private void Start()
+    {
+        player = GameObject.Find("玩家").transform;
+    }
+    private void LateUpdate()       //在update之後執行
+    {
+        track();
+    }
+    /// <summary>
+    /// 追蹤玩家座標方法
+    /// </summary>
+    private void track() 
+    {
+        Vector3 posPlayer = player.position; //玩家.座標
+        Vector3 posCamera = transform.position; //攝影機.座標
+
+        posPlayer.x = 0;        //x=0
+
+        posPlayer.z = Mathf.Clamp(posPlayer.z, top, bottom);//將z軸限制住
+
+        transform.position= Vector3.Lerp(posPlayer, posCamera, 0.5f*Time.deltaTime*speed);
+    }
+}
--- Unity Project/Assets/Scripts/Player.cs
+++ Unity Project/Assets/Scripts/Player.cs
@@ -6,12 +6,16 @@ public class Player : MonoBehaviour
 {
     private Joystick joystick;
     private Rigidbody rig;
-
+    private Animator ani;
+    private Transform target;
+    [Header("移動速度"),Range(1,300)]
     public float speed = 15;
     private void Start()
     {
+        ani = GetComponent<Animator>();
         rig = GetComponent<Rigidbody>();
         joystick = GameObject.Find("虛擬搖桿").GetComponent<Joystick>();
+        target = GameObject.Find("目標").transform;
     }
     private void FixedUpdate()
     {
@@ -25,5 +29,15 @@ public class Player : MonoBehaviour
         float h = joystick.Horizontal;     //水平
         float v = joystick.Vertical;       //垂直 
         rig.AddForce(-h*speed, 0, -v*speed);
+        ani.SetBool("跑步開關",h != 0 || v != 0);
+        Vector3 PosPlayer = transform.position;  //玩家,座標
+        Vector3 PosTarget = new Vector3(PosPlayer.x - h, 0.26f, PosPlayer.z -v);
+        //print(PosTarget);
+        target.position = PosTarget;  //目標.座標=計算後的座標
+
+        PosTarget.y = PosPlayer.y;
+
+        transform.LookAt(PosTarget);
+
     }
 }
