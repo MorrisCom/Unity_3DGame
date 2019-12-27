e6b1b482e469072c4aeecd0d7d6ab9ec13fb17d6
MorrisCom <57181186+MorrisCom@users.noreply.github.com>
2019-12-27 20:31:59 +0800

    補課12/25

---
+++ Unity Project/Assets/動畫/Scripts/LearnArray.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class LearnArray : MonoBehaviour
+{
+    public int pA = 10;
+    public int pB = 20;
+    public int pC = 30;
+    //類型、陣列
+    public int[] players;
+    public float[] speeds;
+    public bool[] missions;
+    public Color[] colors;
+
+    public string[] names = new string[5];
+    public string[] animals = { "dog", "cat", "monkey" };
+}
